a385b1fd0ffc1ed6d80052ddff09d9f3292f2b34
Abhishek <abhishek.km010@gmail.com>
2025-04-10 13:37:04 +0530

    add sercurity config detals from spring documentation

--- src/main/java/dev/abhi/userservice/userservice/Security/EncoderConfig.java
+++ src/main/java/dev/abhi/userservice/userservice/Security/EncoderConfig.java
@@ -1,4 +1,4 @@
-package dev.abhi.userservice.userservice.configs;
+package dev.abhi.userservice.userservice.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
